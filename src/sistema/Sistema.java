175695915ee5f69537f7b44716889355e2f6d1d6
LorennaFrancelino <lorenna.rj@hotmail.com>
2016-05-08 23:32:26 -0300

    sem codec jar

--- src/sistema/Sistema.java
+++ src/sistema/Sistema.java
@@ -5,9 +5,6 @@ import telas.*;
 import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-
 
 public class Sistema {
 
